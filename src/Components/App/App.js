e4a7b0f916655bf15f76f585d66584a43e555f23
Rajakumaran C <rajakumaran0908@gmail.com>
2021-04-07 16:19:09 -0400

    Completed Seciton 8

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -31,6 +31,7 @@ class App extends React.Component{
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   addTrack(track){
@@ -58,6 +59,10 @@ class App extends React.Component{
     });
   }
 
+  savePlaylist(){
+    const trackURIs = this.state.playlistTracks.map(track => track.uri);
+  }
+
   render(){
     return (
       <div>
@@ -66,7 +71,7 @@ class App extends React.Component{
         <SearchBar />
         <div className="App-playlist">
           <SearchResults searchResults = {this.state.searchResults} onAdd = {this.addTrack} /> 
-          <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} /> 
+          <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist} /> 
         </div>
       </div>
     </div>
